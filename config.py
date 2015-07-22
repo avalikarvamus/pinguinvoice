589c3cfa71c02c51485b102b7b09cb1809c3fe87
Madis Veskimeister <madis@pingviinitiivul.ee>
2015-07-22 21:20:31 +0300

    Removal of some accidental copies.

--- config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os
-
-DEBUG = True
-TESTING = True
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'invoices.db')
