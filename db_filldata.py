22a929bd361444835731707fc9b2d5d4173878e1
Madis Veskimeister <madis@pingviinitiivul.ee>
2018-06-10 11:52:38 +0200

    Updated copiright

--- db_filldata.py
+++ db_filldata.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#    Copyright 2015 Madis Veskimeister <madis@pingviinitiivul.ee>
+#    Copyright 2015-2018 Madis Veskimeister <madis@pingviinitiivul.ee>
 #
 
 from app.config import SQLALCHEMY_DATABASE_URI
